ea9e963a941375fb7215a479d9fc9b26366b7071
villivald <maxim.villivald@gmail.com>
2021-06-24 19:57:20 +0300

    fix locations

--- pages/books.js
+++ pages/books.js
@@ -1,6 +1,6 @@
 import { connectToDatabase } from "../util/mongodb";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Books({ books }) {
   return (
