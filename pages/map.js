b0f6124ae67f5962f4116ba1da9e13c5c3b49029
villivald <maxim.villivald@gmail.com>
2021-06-25 18:19:27 +0300

    add mapbox

--- pages/map.js
+++ pages/map.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const map = () => {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
   return (
     <div>
       <Header />
-      MAP
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.ACCESS_TOKEN}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
+
       <Footer />
     </div>
   );
