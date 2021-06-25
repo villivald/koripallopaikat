9b43b3e8b07273ac57bc3a9311b91a1df2041d0c
villivald <maxim.villivald@gmail.com>
2021-06-25 19:13:03 +0300

    fix map

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../index.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/map.js
+++ pages/map.js
@@ -1,27 +1,45 @@
-import React from "react";
-import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import React, { useRef, useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import mapboxgl from "!mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidmlsbGl2YWxkIiwiYSI6ImNrcWNnYjRvdDFqaTUyd212NHQzdGN5cGkifQ.Y98cfsnc0_V4f1-6El0Mhw";
 
 const map = () => {
-  const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(24.96);
+  const [lat, setLat] = useState(60.2);
+  const [zoom, setZoom] = useState(10);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
   });
 
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
   return (
     <div>
       <Header />
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={process.env.ACCESS_TOKEN}
-        onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      />
-
+      <div>
+        <div className="sidebar">
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
       <Footer />
     </div>
   );
