05f7264a80028c1c38c714de1a311fb0c00d6a46
villivald <maxim.villivald@gmail.com>
2021-07-02 13:05:26 +0300

    add surface image onHover

--- components/Court.js
+++ components/Court.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-// import Image from "next/image";
-// import PIC from "../public/vercel.svg";
+import ReportOutlinedIcon from "@material-ui/icons/ReportOutlined";
+import ImageOutlinedIcon from "@material-ui/icons/ImageOutlined";
+import LinkIcon from "@material-ui/icons/Link";
+import PIC from "../public/vercel.svg";
 
 const Court = ({ court }) => {
+  const [isShown, setIsShown] = useState(false);
   return (
     <Card className="basketballField">
       <CardMedia
@@ -21,15 +25,32 @@ const Court = ({ court }) => {
           Address: {court.address}
         </Typography>
         <Typography color="textSecondary">
-          Surface: {court.surface} ❓
+          Surface: {court.surface}{" "}
+          <span
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}
+            style={{ cursor: "pointer" }}
+          >
+            ❓
+          </span>
         </Typography>
-        {/* <Image src={PIC} alt="Picture of the author" width={50} height={50} /> */}
+        {isShown && (
+          <div className="hoverPicture">
+            <Image src={PIC} alt="court surface" width={100} height={100} />
+          </div>
+        )}
         <Typography color="textSecondary">Place: {court.type}</Typography>
         <Typography color="textSecondary">Baskets: {court.baskets}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" href={court.link} style={{ marginLeft: "-7px" }}>
-          Link
+        <Button size="small" href={court.link}>
+          <LinkIcon />
+        </Button>
+        <Button size="small" href={court.pic}>
+          <ImageOutlinedIcon />
+        </Button>
+        <Button size="small" href={"mailto:maxim@villivald.com"}>
+          <ReportOutlinedIcon />
         </Button>
       </CardActions>
     </Card>
