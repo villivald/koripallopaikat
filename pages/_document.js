a67f4063ec47e1d049434d3323e23476b5c18bd1
villivald <maxim.villivald@gmail.com>
2021-07-08 21:27:25 +0300

    add preloading first image on main page

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+          <link
+            rel="preload"
+            as="image"
+            href="/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fimages%2F1.4c0ce015901e9e41811654393844f48d.webp&w=1080&q=75"
+          />
           <Main />
           <NextScript />
         </body>
