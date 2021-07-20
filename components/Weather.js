b1bea25a431aaa86be900cf1080a5ca379569d67
villivald <maxim.villivald@gmail.com>
2021-07-20 14:13:22 +0300

    add '+' and title to weather component

--- components/Weather.js
+++ components/Weather.js
@@ -6,10 +6,14 @@ const Weather = ({ weather }) => {
       {weather && (
         <div className="weather">
           <h2>{weather.name}</h2>
-          <h2>{(weather.main.temp - 273.15).toFixed(2)} °​C</h2>
+          <h2>
+            {weather.main.temp > 273.15 && "+"}
+            {(weather.main.temp - 273.15).toFixed(2)} °​C
+          </h2>
           <img
             alt={weather.weather[0].description}
             src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            title={weather.weather[0].description}
             height={50}
             width={50}
           />
