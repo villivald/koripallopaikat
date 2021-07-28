9b40c3bec493a777ee20ceb905a2ac339b56afc1
villivald <maxim.villivald@gmail.com>
2021-07-28 11:16:41 +0300

    add react-ga

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-203458424-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -16,6 +20,7 @@ class MyDocument extends Document {
             as="image"
             href="/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fimages%2F1.4c0ce015901e9e41811654393844f48d.webp&w=1080&q=75"
           />
+
           <Main />
           <NextScript />
         </body>
