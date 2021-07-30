c2673f1298eb639f3ca4feadb9296b224c278f0e
villivald <maxim.villivald@gmail.com>
2021-07-30 18:16:42 +0300

    add pwa

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,18 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
+
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#317EFB" />
+
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=UA-203458424-1"
