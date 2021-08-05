06293bda9074773581ba49668851c7a8ebc9963c
villivald <maxim.villivald@gmail.com>
2021-08-05 21:29:14 +0300

    add sort & filter to list page

--- pages/courts/index.js
+++ pages/courts/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import Head from "next/head";
-import Box from "@material-ui/core/Box";
+import { Switch, TextField, Box } from "@material-ui/core";
 import Pagination from "@material-ui/lab/Pagination";
 import { connectToDatabase } from "../../util/mongodb";
 
@@ -12,10 +12,18 @@ const index = ({ courts }) => {
   const itemsPerPage = 8;
   const [page, setPage] = useState(1);
   const [numberOfPages] = useState(Math.ceil(courts.length / itemsPerPage));
+  const [filter, setFilter] = useState(null);
+  const [sortByBaskets, setSortByBaskets] = useState(false);
+  const [sortBySurface, setSortBySurface] = useState(false);
+  const [sortByAddress, setSortByAddress] = useState(false);
 
   const handleChange = (event, value) => {
     event.preventDefault();
     setPage(value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   };
   return (
     <div>
@@ -24,8 +32,36 @@ const index = ({ courts }) => {
         <link rel="icon" href="favicons/favicon.ico" />
       </Head>
       <Header />
+      <div>
+        <h2 className="sortFilterTitle">Filter by</h2>
+        <div className="sortFilter">
+          <TextField
+            onChange={handleFilterChange}
+            type="text"
+            placeholder="Merimiehenkatu, 9"
+            label="Address"
+          />
+        </div>
+        <h2 className="sortFilterTitle">Sort by</h2>
+        <div className="sortFilter">
+          Baskets:
+          <Switch onChange={() => setSortByBaskets(!sortByBaskets)} />
+          Surface type:
+          <Switch onChange={() => setSortBySurface(!sortBySurface)} />
+          Address:
+          <Switch onChange={() => setSortByAddress(!sortByAddress)} />
+        </div>
+      </div>
       <div className="listCourt">
         {courts
+          .filter((court) =>
+            filter
+              ? court.address.toLowerCase().includes(filter.toLowerCase())
+              : court
+          )
+          .sort((max, min) => sortByBaskets && min.baskets - max.baskets)
+          .sort((a, b) => sortBySurface && (a.surface > b.surface ? 1 : -1))
+          .sort((a, b) => sortByAddress && (a.address > b.address ? 1 : -1))
           .slice((page - 1) * itemsPerPage, page * itemsPerPage)
           .map((court) => (
             <CourtList court={court} key={court._id} />
