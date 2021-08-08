57f533e9921f34cf4182ed58965c4ecc9e31ae33
villivald <maxim.villivald@gmail.com>
2021-08-08 14:05:12 +0300

    add bottom navigation to main page

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,10 @@
-import Link from "next/link";
 import MainPageCourt from "./MainPageCourt";
 import mainPageCourts from "../data/mainPageCourts";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import MapOutlinedIcon from "@material-ui/icons/MapOutlined";
 
 const main = () => {
   return (
@@ -10,9 +14,26 @@ const main = () => {
           <MainPageCourt court={court} key={court._id} />
         ))}
       </div>
-      <div className="mainPageLink">
-        <Link href={"/courts"}>Full List</Link>
-      </div>
+      <BottomNavigation showLabels className="mainPageLink">
+        <BottomNavigationAction
+          href={"/map"}
+          label="Map"
+          className="mainPageLinkIcon"
+          icon={<MapOutlinedIcon fontSize="inherit" />}
+        />
+        <BottomNavigationAction
+          href={"/courts"}
+          label="Full List"
+          className="mainPageLinkIcon"
+          icon={<FormatListBulletedIcon fontSize="inherit" />}
+        />
+        <BottomNavigationAction
+          href={"/add"}
+          label="Add"
+          className="mainPageLinkIcon"
+          icon={<AddCircleOutlineIcon fontSize="inherit" />}
+        />
+      </BottomNavigation>
     </>
   );
 };
