e93d23dcee1d6887e3728642a31b449034121bf5
villivald <maxim.villivald@gmail.com>
2021-08-12 09:44:17 +0300

    add new court (turku)

--- pages/map.js
+++ pages/map.js
@@ -55,7 +55,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat100821"],
+        layers: ["koripallopaikat120821"],
       });
       if (!features.length) {
         return;
