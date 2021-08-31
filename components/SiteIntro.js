3b3ad74fb7f61418432e674d1a0f1eed489188ab
villivald <maxim.villivald@gmail.com>
2021-08-31 19:15:12 +0300

    add cat pic to intro

--- components/SiteIntro.js
+++ components/SiteIntro.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import basket from "../public/blob.svg";
 
 const SiteIntro = () => {
   return (
     <div className="intro">
+      <Link href="https://villivald.com">
+        <img
+          src="https://villivald.com/static/media/dc2.d57757c1.webp"
+          alt="court surface"
+          width={200}
+          height={150}
+          className="cat"
+        />
+      </Link>
       <h4 className="introText">
         <Image
           className="introImage"
