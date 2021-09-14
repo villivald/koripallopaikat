b3eb3e48e33acd91008c3bc7c07eef9a3eb6ee55
villivald <maxim.villivald@gmail.com>
2021-09-14 18:04:35 +0300

    add cat tooltip on hover

--- components/SiteIntro.js
+++ components/SiteIntro.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import basket from "../public/blob.svg";
@@ -6,15 +5,22 @@ import basket from "../public/blob.svg";
 const SiteIntro = () => {
   return (
     <div className="intro">
-      <Link href="https://villivald.com">
-        <img
-          src="https://villivald.com/static/media/dc2.d57757c1.webp"
-          alt="court surface"
-          width={200}
-          height={150}
-          className="cat"
-        />
-      </Link>
+      <div className="toolTipCat">
+        <Link href="https://villivald.com">
+          <img
+            src="https://villivald.com/static/media/dc2.d57757c1.webp"
+            alt="cat logo"
+            width={200}
+            height={150}
+            className="cat"
+          />
+        </Link>
+        <Link href="https://villivald.com">
+          <span class="toolTipText">
+            Psst, Hey You. Wanna see where I came from?{" "}
+          </span>
+        </Link>
+      </div>
       <h4 className="introText">
         <Image
           className="introImage"
