2963991acad5553015cf1c584c1ab09e3bb6a50b
villivald <maxim.villivald@gmail.com>
2021-09-20 17:36:45 +0300

    add new court (Vuosaaren Urheilukenttä)

--- pages/map.js
+++ pages/map.js
@@ -55,7 +55,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat170921"],
+        layers: ["koripallopaikat200921"],
       });
       if (!features.length) {
         return;
