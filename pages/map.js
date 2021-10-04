9a9a2dde64f611133205f8c1f4f7c52f5042c18f
villivald <maxim.villivald@gmail.com>
2021-10-04 14:09:24 +0300

    add new court

--- pages/map.js
+++ pages/map.js
@@ -55,7 +55,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat011021"],
+        layers: ["koripallopaikat041021"],
       });
       if (!features.length) {
         return;
