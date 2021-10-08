0cbb9a16483482fdd5a2b66efb192df80ff25b1a
villivald <maxim.villivald@gmail.com>
2021-10-08 21:09:27 +0300

    add new courts (roihuvuori)

--- pages/map.js
+++ pages/map.js
@@ -55,7 +55,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat041021"],
+        layers: ["koripallopaikat081021"],
       });
       if (!features.length) {
         return;
