64580615bb23543f2527c2adadf4dfa6fa86c4a4
villivald <maxim.villivald@gmail.com>
2021-10-27 11:10:11 +0300

    add new courts (aurinkolahti)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat081021"],
+        layers: ["koripallopaikat271021"],
       });
       if (!features.length) {
         return;
