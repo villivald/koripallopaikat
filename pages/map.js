6dbe6cb86dff810643e4446893a00755687eb729
villivald <maxim.villivald@gmail.com>
2021-11-01 19:39:42 +0200

    add new court (etu-töölö)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat301021"],
+        layers: ["koripallopaikat011121"],
       });
       if (!features.length) {
         return;
