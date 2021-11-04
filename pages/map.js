db72d84b1f08361dc6378e8a7e80853bb2e95871
villivald <maxim.villivald@gmail.com>
2021-11-04 10:54:57 +0200

    add new court (itäkeskus)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat021121"],
+        layers: ["koripallopaikat041121"],
       });
       if (!features.length) {
         return;
