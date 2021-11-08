3783c815c08da8810b0ded39db1e78c55146b757
villivald <maxim.villivald@gmail.com>
2021-11-08 17:34:43 +0200

    new court is added (kallahti)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat061121"],
+        layers: ["koripallopaikat081121"],
       });
       if (!features.length) {
         return;
