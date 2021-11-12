bd31cdda63c1ca6dfbf5922567cc84eb92a659a4
villivald <maxim.villivald@gmail.com>
2021-11-12 15:17:20 +0200

    add kbar - initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,17 @@ import "../css/search.css";
 import "../css/review.css";
 import "../css/news.css";
 import "mapbox-gl/dist/mapbox-gl.css";
+import {
+  KBarProvider,
+  KBarPortal,
+  KBarPositioner,
+  KBarAnimator,
+  KBarSearch,
+  KBarResults,
+  useMatches,
+  NO_GROUP,
+} from "kbar";
+import { useMemo } from "react";
 
 import Router from "next/router";
 import NProgress from "nprogress";
@@ -25,10 +36,76 @@ Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+const RenderResults = () => {
+  const groups = useMatches();
+  const flattened = useMemo(
+    () =>
+      groups.reduce((acc, curr) => {
+        acc.push(curr.name);
+        acc.push(...curr.actions);
+        return acc;
+      }, []),
+    [groups]
+  );
+
   return (
-    <AppWrapper>
-      <Component {...pageProps} />
-    </AppWrapper>
+    <KBarResults
+      items={flattened.filter((i) => i !== NO_GROUP)}
+      onRender={({ item, active }) =>
+        typeof item === "string" ? (
+          <div>{item}</div>
+        ) : (
+          <div
+            style={{
+              background: active ? "#eee" : "transparent",
+            }}
+          >
+            {item.name}
+          </div>
+        )
+      }
+    />
+  );
+};
+
+export default function MyApp({ Component, pageProps }) {
+  const actions = [
+    {
+      id: "home",
+      name: "Home",
+      shortcut: ["h"],
+      keywords: "home page",
+      perform: () => (window.location.pathname = "/"),
+    },
+    {
+      id: "blog",
+      name: "Map",
+      shortcut: ["b"],
+      keywords: "map",
+      perform: () => (window.location.pathname = "map"),
+    },
+    {
+      id: "contact",
+      name: "List",
+      shortcut: ["c"],
+      keywords: "list of courts",
+      perform: () => (window.location.pathname = "courts"),
+    },
+  ];
+  return (
+    <KBarProvider actions={actions}>
+      <KBarPortal>
+        <KBarPositioner>
+          <KBarAnimator>
+            <KBarSearch />
+            <RenderResults />
+          </KBarAnimator>
+        </KBarPositioner>
+      </KBarPortal>
+
+      <AppWrapper>
+        <Component {...pageProps} />
+      </AppWrapper>
+    </KBarProvider>
   );
 }
