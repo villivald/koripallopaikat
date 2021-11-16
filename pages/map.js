85815281c9e15d7d4b9a09191dbb6fbf409293d9
villivald <maxim.villivald@gmail.com>
2021-11-16 17:10:25 +0200

    add new court (länsimäki)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat121121"],
+        layers: ["koripallopaikat161121"],
       });
       if (!features.length) {
         return;
