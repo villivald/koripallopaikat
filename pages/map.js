acd4912572ed2421174945bce8c43baf8ecb33d8
villivald <maxim.villivald@gmail.com>
2021-11-19 12:51:47 +0200

    add new court (tampere)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat161121"],
+        layers: ["koripallopaikat191121"],
       });
       if (!features.length) {
         return;
