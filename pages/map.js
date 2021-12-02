bf0402df76fdc5a3221d41212c0650fde0ff8521
villivald <maxim.villivald@gmail.com>
2021-12-02 21:27:47 +0200

    add new court (itä-pakila)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat291121"],
+        layers: ["koripallopaikat021221"],
       });
       if (!features.length) {
         return;
