def25b8dd487a8a2e0c007da9e8162f047db09de
villivald <maxim.villivald@gmail.com>
2021-12-07 11:26:19 +0200

    add new court (oulunkylä)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat021221"],
+        layers: ["koripallopaikat071221"],
       });
       if (!features.length) {
         return;
