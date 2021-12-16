f911a8bcec5fe5ad51d0987922fc7526863801d4
villivald <maxim.villivald@gmail.com>
2021-12-16 22:59:02 +0200

    add new court (pikkukoski)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat151221"],
+        layers: ["koripallopaikat161221"],
       });
       if (!features.length) {
         return;
