7793fd828e4d7f01d2e42b116f4b4bb6e03c9138
villivald <maxim.villivald@gmail.com>
2022-04-25 21:33:58 +0300

    add new court (lahti, harjukatu)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat230422"],
+        layers: ["koripallopaikat250422"],
       });
       if (!features.length) {
         return;
