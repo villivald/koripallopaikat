945ca5e665703f8541358a3ab91bfce44ae8ab11
villivald <maxim.villivald@gmail.com>
2022-06-19 10:05:55 +0300

    add new court (töölön koulu)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat270522"],
+        layers: ["koripallopaikat190622"],
       });
       if (!features.length) {
         return;
