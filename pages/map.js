b27a79b1dc51f563e0f893876d889c8577272340
villivald <maxim.villivald@gmail.com>
2022-06-25 13:45:43 +0300

    add new court (keskuspuisto)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat190622"],
+        layers: ["koripallopaikat250622"],
       });
       if (!features.length) {
         return;
