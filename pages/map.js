f602b8557c87890c504c1145cf5b8cf28a22265b
villivald <maxim.villivald@gmail.com>
2022-07-04 22:20:40 +0300

    add new court (käpylä - osmontie)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat250622"],
+        layers: ["koripallopaikat040722"],
       });
       if (!features.length) {
         return;
