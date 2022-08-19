c10c18fd11f4b45e8e5f6dda26d033eb571d8b65
villivald <maxim.villivald@gmail.com>
2022-08-19 21:01:58 +0300

    add new court (fiskars - koulu)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat240722"],
+        layers: ["koripallopaikat190822"],
       });
       if (!features.length) {
         return;
