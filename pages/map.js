b399ac6136440762678fc6745f61e1f15ef7b2d6
villivald <maxim.villivald@gmail.com>
2022-08-24 22:02:27 +0300

    add new court (lohja - asemakatu)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat190822"],
+        layers: ["koripallopaikat240822"],
       });
       if (!features.length) {
         return;
