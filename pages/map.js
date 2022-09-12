4deba39aa7c9b1c66078e5fd8f7f419f4fbc42a4
villivald <maxim.villivald@gmail.com>
2022-09-12 17:00:45 +0300

    add new court (lohja -  Linnaistenkatu)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat270822"],
+        layers: ["koripallopaikat120922"],
       });
       if (!features.length) {
         return;
