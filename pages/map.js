33d25e737124cf4a26c5a040257bc5c925734a7f
villivald <maxim.villivald@gmail.com>
2022-09-16 19:24:40 +0300

    add new court - niittykumpu 2 baskets

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat140922"],
+        layers: ["koripallopaikat160922"],
       });
       if (!features.length) {
         return;
