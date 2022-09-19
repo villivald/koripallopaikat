bea83b3c41670c50ee89eadd461da9a205da0aed
villivald <maxim.villivald@gmail.com>
2022-09-19 21:39:53 +0300

    add new court - matinkylä (ruotsalainen koulu)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["koripallopaikat160922"],
+        layers: ["koripallopaikat190922"],
       });
       if (!features.length) {
         return;
