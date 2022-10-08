10bbed4da6276e3505a73efc4fcd9d46a3ad9266
villivald <maxim.villivald@gmail.com>
2022-10-08 21:08:13 +0300

    add new court (haukilahti - outdoor basketball court)

--- pages/map.js
+++ pages/map.js
@@ -56,7 +56,7 @@ const map = () => {
   useEffect(() => {
     map.current.on("click", function (e) {
       var features = map.current.queryRenderedFeatures(e.point, {
-        layers: ["031022"],
+        layers: ["081022"],
       });
       if (!features.length) {
         return;
