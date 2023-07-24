d784b5769960f750a21ff4f9a484b8b54f1fe4e8
villivald <maxim.villivald@gmail.com>
2023-07-24 16:11:04 +0300

    remove redundant links from contact page

--- data/contactLinks.js
+++ data/contactLinks.js
@@ -1,40 +1,15 @@
-import react from "../public/icons/react.svg";
 import telegram from "../public/icons/telegram.svg";
-import twitter from "../public/icons/twitter.svg";
-import facebook from "../public/icons/facebook.svg";
 import instagram from "../public/icons/instagram.svg";
-import hackernoon from "../public/icons/hackernoon.svg";
 import gmail from "../public/icons/gmail.svg";
 import github from "../public/icons/github-icon.svg";
 import dev from "../public/icons/dev-badge.svg";
 import buyme from "../public/icons/buyme.svg";
-import eleventy from "../public/icons/11ty.webp";
 
 const contactLinks = [
-  {
-    link: "https://villivald.com",
-    alt: "react",
-    src: react,
-  },
-  {
-    link: "https://create-react-app.com",
-    alt: "eleventy",
-    src: eleventy,
-  },
   {
     link: "https://github.com/villivald",
     alt: "github",
     src: github,
-  },
-  {
-    link: "https://twitter.com/crapp_blog",
-    alt: "twitter",
-    src: twitter,
-  },
-  {
-    link: "https://www.facebook.com/koripallopaikat",
-    alt: "facebook",
-    src: facebook,
   },
   {
     link: "https://www.instagram.com/koripallopaikat/",
@@ -45,11 +20,6 @@ const contactLinks = [
     link: "https://dev.to/villivald",
     alt: "dev.to",
     src: dev,
-  },
-  {
-    link: "https://hackernoon.com/u/villivald",
-    alt: "hackernoon",
-    src: hackernoon,
   },
   {
     link: "https://t.me/create_react_app",
